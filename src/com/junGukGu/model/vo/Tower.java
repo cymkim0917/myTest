24c8085fa97e7772271a23ac11bb6f09a5c2d2a0
cymkim0917 <cymkim0917@naver.com>
2019-02-10 15:39:07 +0900

    write Tower

--- src/com/junGukGu/model/vo/Tower.java
+++ src/com/junGukGu/model/vo/Tower.java
@@ -1,5 +1,12 @@
 package com.junGukGu.model.vo;
 
 public class Tower {
-
+	private int attackPoint;
+	private double attackSpee;
+	private int attackRange;
+	private int level;
+	
+	public Tower() {
+		System.out.println("채연이가 만든 타워입니다");
+	}
 }
