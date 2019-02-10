0c703dbdf25a95e463f55489a1ee26d8dcf0426a
박상언 <@DESKTOP-7T6L2J9>
2019-02-10 16:38:40 +0900

    test

--- src/com/junGukGu/model/vo/Missile.java
+++ src/com/junGukGu/model/vo/Missile.java
@@ -2,7 +2,7 @@ package com.junGukGu.model.vo;
 
 public class Missile {
 	public void test() {
-		System.out.println("성공성공성공");
-		System.out.println("????");
+		System.out.println("test");
+		System.out.println("test1");
 	}
 }
